7caafdaedf38b0c73e0f324a484dd9409716c9f9
Anchu Lee <etirps@drowmac.local>
2019-01-27 11:14:23 -0500

    Initial Commit

---
+++ Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by Anchu Lee on 1/27/19.
+//  Copyright © 2019 Anchu Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by Anchu Lee on 1/27/19.
+//  Copyright © 2019 Anchu Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
