0e93ecf9ea70636b94a93a0ff82b91dbbf494eae
Anchu Lee <etirps@drowmac.local>
2019-01-28 10:01:13 -0500

    added comments, disabled unused code

--- Hello/ViewController.m
+++ Hello/ViewController.m
@@ -23,12 +23,14 @@ static int tapTotal;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    greetings = [NSArray arrayWithObjects:@"Hello!", @"¡Hola!", @"مرحبا!", @"שלום!", @"你好!", @"Hallo!", @"こんにちは!", @"สวัสดี!", @"여보세요!", @"звать!", @"Salut!", @"Witaj!", @"Cheerio!", @"o/", @"Wassup!", @"Zdravo!", @"Привіт!", @"ဟယ်လို!", @"Xin Chào!", @"Olá!", @"Ahoj!", @"سلام!", @"Dia Dhuit!", @"Hej!", @"nuqneH!", @"G'day Mate!", @"\(•◡•)/", @"HEWWO!!?", nil];
+    greetings = [NSArray arrayWithObjects:@"Hello!", @"¡Hola!", @"مرحبا!", @"שלום!", @"你好!", @"Hallo!", @"こんにちは!", @"สวัสดี!", @"여보세요!", @"звать!", @"Salut!", @"Witaj!", @"Cheerio!", @"o/", @"Wassup!", @"Zdravo!", @"Привіт!", @"ဟယ်လို!", @"Xin Chào!", @"Olá!", @"Ahoj!", @"سلام!", @"Dia Dhuit!", @"Hej!", @"nuqneH!", @"G'day Mate!", @"\\(•◡•)/", @"HEWWO!!?", nil];
     colors = [NSArray arrayWithObjects: [UIColor redColor], [UIColor orangeColor], [UIColor yellowColor], [UIColor greenColor], [UIColor blueColor], [UIColor purpleColor], nil];
     greetingIndex = 1;
     colorCounter = 1;
     tapTotal = 0;
     
+    // Set up tap detector
+    // Using long press gesture set to 0 to grab press-down
     UILongPressGestureRecognizer *tap = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(onTap:)];
     [tap setMinimumPressDuration:0];
     [self.view addGestureRecognizer:tap];
@@ -36,6 +38,7 @@ static int tapTotal;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+/*
 - (IBAction)changeMessage:(id)sender {
     
     [greetLabel setText:[greetings objectAtIndex:greetingIndex]];
@@ -43,10 +46,14 @@ static int tapTotal;
     if (greetingIndex >= [greetings count]) {
         greetingIndex = 0;
     }
-}
+}*/
 
+// Method called when tapped
 - (void)onTap:(UITapGestureRecognizer *)recognizer {
+    // Check if call is from initial touch
     if (recognizer.state == UIGestureRecognizerStateBegan) {
+        
+        // Tap counter code
         tapTotal++;
         if(tapTotal >= 10 && [tapLabel isHidden])
         {
@@ -54,36 +61,44 @@ static int tapTotal;
             [tapCounter setHidden:false];
         }
         
+        // Animate labels
         [UIView animateWithDuration:0.1 animations:^{
             self->greetLabel.transform = CGAffineTransformScale(self->greetLabel.transform, 0.75, 0.75);
             self->tapCounter.transform = CGAffineTransformScale(self->tapCounter.transform, 0.90, 0.90);
         
         } completion:^(BOOL finished) {
+            // Update counter
             [self->tapCounter setText:[NSString stringWithFormat:@"%d", tapTotal]];
             
+            // Continue animation
             [UIView animateWithDuration:0.1 animations:^{
                 self->greetLabel.transform = CGAffineTransformScale(self->greetLabel.transform, 1/0.75, 1/0.75);
                 self->tapCounter.transform = CGAffineTransformScale(self->tapCounter.transform, 1/0.90, 1/0.90);
             }];
         }];
         
+        // Drop animation
         [self dropText];
     }
 }
 
 - (void)dropText {
+    // Get screen dimensions
     CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
     CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
     
-    // find position
+    // find left/right adjustment for new label
     int adjustment = arc4random_uniform(100) - 50;
     
-    // Create Label that falls directly from the button
+    // Create Label right on top of the greet label
     UILabel *fallLabel = [[UILabel alloc] initWithFrame:CGRectMake(0 + adjustment,303, 375, 60)];
+    // Make it the content as the greet label
     [fallLabel setText: [greetLabel text]];
     [fallLabel setTextColor:[greetLabel textColor]];
-    [fallLabel setFont:[UIFont systemFontOfSize:40]];
     fallLabel.textAlignment = NSTextAlignmentCenter;
+    // Set font to be smaller
+    [fallLabel setFont:[UIFont systemFontOfSize:40]];
+    // Add label to the view
     [[self view] addSubview:fallLabel];
     
     // Set button to next message
@@ -101,8 +116,8 @@ static int tapTotal;
 - (void) nextMessage {
     // Set title to a variable in the array
     [greetLabel setText:[greetings objectAtIndex:greetingIndex]];
+    // Set colors of the labels
     [greetLabel setTextColor:[colors objectAtIndex:colorCounter]];
-    
     [tapLabel setTextColor:[colors objectAtIndex:colorCounter]];
     [tapCounter setTextColor:[colors objectAtIndex:colorCounter]];
     
@@ -122,6 +137,7 @@ static int tapTotal;
     }
 }
 
+/*
 - (IBAction)spinButton:(id)sender {
     
     CABasicAnimation *fullRotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
@@ -129,6 +145,6 @@ static int tapTotal;
     fullRotation.duration = 0.5;
     
     [self.greetLabel.layer addAnimation:fullRotation forKey:@"myRotationAnimation"];
-}
+}*/
 
 @end
